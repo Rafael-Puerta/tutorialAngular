8a91199e074ebe01218330d7bbba86d975cbfb56
AzureAD\RafaelPuertaChamorro <rpuerta@5skill.com>
2022-10-25 12:24:11 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tutorialAngular';
+  newMemberName="";
+  members:string[]=[];
+  errorMessage:string="";
+  numberTeams:number|string="";
+  teams: string[][]=[];
+  createGroups(){
+    if(!this.numberTeams||this.numberTeams<=0){
+      this.errorMessage="Invalid number of teams";
+      return;
+    }
+    if(this.members.length<this.numberTeams){
+      this.errorMessage="Not enough members to make the teams";
+      return;
+    }
+    this.teams=[];
+    this.errorMessage=""
+    const allMembers=[...this.members];
+    while(allMembers.length){
+      for (let i=0;i<this.numberTeams;i++){
+        const randomIdex=Math.floor(Math.random()*allMembers.length);
+        const member=allMembers.splice(randomIdex,1)[0];
+        if(!member){
+          break;
+        }
+        if(this.teams[i]){
+          this.teams[i].push(member);
+        } else{
+          this.teams[i]=[member];
+        }
+      }
+    }
+    
+    this.members=[];
+    this.numberTeams="";
+  }
+  addMember(){
+    if(!this.newMemberName){
+      this.errorMessage="Name cant be empty";
+      return;
+    }
+    this.members.push(this.newMemberName);
+    this.errorMessage="";
+    this.newMemberName="";
+  }
+  onInput(member:string){
+    this.newMemberName=member;
+  }
+  onInputNumber(teamsNum:string){
+    this.numberTeams=Number(teamsNum);
+  }
 }
